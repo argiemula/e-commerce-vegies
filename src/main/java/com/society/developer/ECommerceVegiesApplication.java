babaaf574580c09a27be5513c57877b0e1a34fe2
Argie <sd.argie.mula.com>
2022-02-04 22:50:42 +0800

    Initial Commit

---
+++ src/main/java/com/society/developer/ECommerceVegiesApplication.java
@@ -0,0 +1,13 @@
+package com.society.developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceVegiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceVegiesApplication.class, args);
+	}
+
+}
